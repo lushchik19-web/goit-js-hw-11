9ce61dc57348291c9aaca7d54ac58450438481fe
Lushshyk Dmytry <lushchik19@gmail.com>
2026-09-12 20:27:33 +0300

    update js code

--- src/main.js
+++ src/main.js
@@ -1,2 +1,48 @@
-// import fetchData from "./js/pixabay-api";
-// import moduleName from "./js/render-functions";
+import getImagesByQuery from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formEl = document.querySelector('.form');
+
+console.log(formEl);
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const inputSubmit = event.currentTarget.elements['search-text'].value;
+
+  console.log(event);
+
+  if (inputSubmit === '') {
+    return;
+  }
+
+  showLoader();
+  clearGallery();
+
+  getImagesByQuery(inputSubmit)
+    .then(value => {
+      if (value.hits.length === 0) {
+        iziToast.show({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+
+      createGallery(value.hits);
+    })
+
+    .finally(hideLoader);
+}
